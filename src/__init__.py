3cdb745670fa1c570ab6c63bbc66ec26d9d29524
EmreY <emre@example.com>
2026-01-11 13:17:14 +0300

    Clean up project files

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 """
 Highway-Env Reinforcement Learning Project
-A capstone project for training autonomous driving agents using PPO.
+A capstone project for training autonomous driving with Deep Q-Networks.
 """
 
 __version__ = "1.0.0"
-__author__ = "Computer Engineering Student"
-__project__ = "Highway-Env Autonomous Driving with RL"
+__author__ = "Emre Y."
+__project__ = "Highway-Env Autonomous Driving with DQN"
